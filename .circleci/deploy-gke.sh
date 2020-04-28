f354c7f31a3d8ee32ee9851c97d979cc994053c2
yusuke39 <hiranoyusuke39@gmail.com>
2020-04-28 09:40:35 +0900

    修正

---
+++ .circleci/deploy-gke.sh
@@ -0,0 +1,4 @@
+##!/bin/bash
+#echo -n ${STG_GCLOUD_SERVICE_KEY} | base64 --decode > gcloud-service-key.json
+#gcloud auth activate-service-account --key-file gcloud-service-key.json
+#gcloud auth configure-docker
